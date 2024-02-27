73c22527d3b6d2011410837362370359d2df0398
vishalBhangare <vishalbhangare2@gmail.com>
2024-02-27 13:02:09 +0530

    1.0.0 (first release)

---
+++ src/data.ts
@@ -0,0 +1,67 @@
+export const Talents = {
+  Talents: {
+    "Talents based on IT and Non-IT": {
+      "IT Talent Categories": {
+        "Software Development": {
+          "Software Engineer": {},
+          "Frontend Developer": {},
+          "Backend Developerr": {},
+          "Full Stack Developer": {},
+          "Mobile App Developer": {},
+        },
+        "Data Science & Analytics": {
+          "Data Scientist": {},
+          "Data Analyst": {},
+        },
+        "Cyber Security": {
+          "Ethical Hacker": {},
+          "Security Engineer": {},
+        },
+        "Cloud Computing": {
+          "Cloud Architect": {},
+          "Cloud Enginee": {},
+          "DevOps Engineer": {},
+        },
+      },
+      "Non-IT Talent Categories": {
+        "Sales & Marketing": {
+          "Sales Representative": {},
+          "Marketing Manager": {},
+        },
+        "Finance & Accounting": {
+          Accountant: {},
+          Auditor: {},
+        },
+        "Human Resources": { "HR Manager": {}, "Recruitment Specialist": {} },
+        "Operations & Logistics": {},
+      },
+    },
+    "Talents based on Experience and Expertise": {
+      "Beginner Level": {},
+      "ntermediate Level": {},
+      "Advance Level": {},
+      "Expert Level": {},
+    },
+  },
+};
+
+export const animalKingdom = {
+  Animalia: {
+    "Tissue level": {
+      Bilateral: {
+        Acoelomates: {},
+        Pseudocoelomates: {},
+        Coelomates: {
+          Annelida: {},
+          Arthropodia: {},
+          Mollusca: {},
+          Echinodermata: {},
+          Hemichordata: {},
+          Chordata: {},
+        },
+      },
+      Radial: { Ctenophora: {}, Coelenterata: {} },
+    },
+    "Cellular level": { Porifera: {} },
+  },
+};
